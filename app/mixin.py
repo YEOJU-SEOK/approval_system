3939b4a7073b2211f8313e4aa66b771c3440bdec
YEOJU-SEOK <yeoju.ac@gmail.com>
2022-05-12 17:18:42 +0900

    add week calendar

--- app/mixin.py
+++ app/mixin.py
@@ -22,6 +22,7 @@ class BaseCalendarMixin:
         week_names.rotate(-self.first_weekday)
         return week_names
 
+
 class MonthCalendarMixin(BaseCalendarMixin):
     def get_previous_month(self, date):
         # 전달 가져옴
@@ -64,3 +65,36 @@ class MonthCalendarMixin(BaseCalendarMixin):
         }
         return calendar_data
 
+
+class WeekCalendarMixin(BaseCalendarMixin):
+    """ 위클리 캘린더 """
+    def get_week_days(self):
+        month = self.kwargs.get('month')
+        year = self.kwargs.get('year')
+        day = self.kwargs.get('day')
+
+        if month and year and day:
+            date = datetime.date(year=int(year), month=int(month), day=int(day))
+        else:
+            date = datetime.date.today()
+
+        for week in self._calendar.monthdatescalendar(date.year, date.month):
+            if date in week:
+                return week
+
+    def get_week_calendar(self):
+        self.setup_calendar()
+        days = self.get_week_days()
+        first = days[0]
+        last = days[-1]
+        calendar_data = {
+            'now': datetime.date.today(),
+            'week_days': days,
+            'week_previous': first - datetime.timedelta(days=7),
+            'week_next': first + datetime.timedelta(days=7),
+            'week_names': self.get_week_names(),
+            # 일주일의 첫날. 마지막날
+            'week_first': first,
+            'week_last': last,
+        }
+        return calendar_data
--- app/views.py
+++ app/views.py
@@ -13,3 +13,13 @@ class MonthCalendar(mixin.MonthCalendarMixin, generic.TemplateView):
         calendar_context = self.get_month_calendar()
         context.update(calendar_context)
         return context
+
+
+class WeekCalendar(mixin.WeekCalendarMixin, generic.TemplateView):
+    template_name = 'app/week.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_week_calendar()
+        context.update(calendar_context)
+        return context
